26ade48a5205083312a3f80542034fdef3fc7d4d
Ivan <88639155+Strife12@users.noreply.github.com>
2022-04-02 23:27:56 +1100

    0.0.3

---
+++ telegram poster bot/main.py
@@ -0,0 +1,32 @@
+import os
+import os.path
+import random
+from PIL import Image, ImageFilter
+import subprocess
+import shutil
+import time
+import sys
+import logging
+import modules
+
+from settings import BOT_TOKEN, admins, channel_ID, channel, srcdir, dscdir
+
+try:
+    modules.choose_file
+
+    modules.choose_caption
+
+    modules.posting_preparation
+
+    modules.double_posting
+
+    modules.run_posting
+except:
+    # перенести в папку с ошибкой
+    next try
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+
